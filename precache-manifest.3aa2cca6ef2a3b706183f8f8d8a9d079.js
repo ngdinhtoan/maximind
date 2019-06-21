22941be42be2a36f3e81951fd19448cebf189264
Toan Nguyen <toan.nguyen@lazada.com>
2019-06-21 17:06:52 +0700

    Updates

---
+++ precache-manifest.3aa2cca6ef2a3b706183f8f8d8a9d079.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5845fe716e702e8f0df5b49d3bd65d",
+    "url": "/maximind/index.html"
+  },
+  {
+    "revision": "23cecf319bbce0b40c91",
+    "url": "/maximind/static/js/2.f15e07d5.chunk.js"
+  },
+  {
+    "revision": "9a23f34b40c8c54362c0",
+    "url": "/maximind/static/js/main.b8545e23.chunk.js"
+  },
+  {
+    "revision": "6fa5354523d41237ce7d",
+    "url": "/maximind/static/js/runtime~main.a841f76a.js"
+  }
+]);
